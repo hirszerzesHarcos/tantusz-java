159f006c03a22918d06e4affe7c908fcffd533fe
Tessaiga <hirszerzesharcos@gmail.com>
2024-08-15 03:05:09 +0200

    milliomos inicializálva.

---
+++ basethings/src/Milliomos.java
@@ -0,0 +1,10 @@
+public class Milliomos {
+  public static void main(String[] args) {
+    double összegASzámlán;
+    összegASzámlán = 5_000;
+    összegASzámlán = összegASzámlán + 40_000_000;
+    System.out.print("A számládon ");
+    System.out.print(összegASzámlán);
+    System.out.println(" Ft van.");
+  }
+}
