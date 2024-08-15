ccdaf0cc6d587c956cda4b2e5d66e3a438ca8bf3
Tessaiga <hirszerzesharcos@gmail.com>
2024-08-15 03:16:19 +0200

    Átalakítva Floatra, amivel ugrott is a pontosság.

--- basethings/src/Milliomos.java
+++ basethings/src/Milliomos.java
@@ -1,8 +1,8 @@
 public class Milliomos {
   public static void main(String[] args) {
-    double összegASzámlán;
-    összegASzámlán = 5_000;
-    összegASzámlán = összegASzámlán + 40_000_000;
+    float összegASzámlán;
+    összegASzámlán = 50.22F;
+    összegASzámlán = összegASzámlán + 4_000_000F;
     System.out.print("A számládon ");
     System.out.print(összegASzámlán);
     System.out.println(" Ft van.");
